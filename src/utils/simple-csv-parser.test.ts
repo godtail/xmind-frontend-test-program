942ca22cd869116beb0b5be0955213a7db5f0841
godtail <godtail@outlook.com>
2020-09-21 19:42:28 +0800

    feat: enhance SimpleCSVParser to support type format

--- src/utils/simple-csv-parser.test.ts
+++ src/utils/simple-csv-parser.test.ts
@@ -1,7 +1,7 @@
-import simpleCsvParser from './simple-csv-parser'
+import simpleCsvParser, { ColumnType } from './simple-csv-parser'
 
 describe('Simple CSV Parser', () => {
-  it('csv parse correctly', () => {
+  it('csv parse correctly (no columnSetting)', () => {
     const csv = `
     name,age
     liming,20
@@ -18,5 +18,38 @@ describe('Simple CSV Parser', () => {
       },
     ]
     expect(simpleCsvParser.parseCsv(csv)).toEqual(parseResult)
+  })
+
+  it('csv parse correctly (with columnSetting)', () => {
+    const csv = `
+    name,age,birthday,money,update
+    liming,20,2020-07-07,534.45,1598009388
+    lili,18,2012-06-12,423.54,1597894280
+    `
+    const columnSetting = {
+      name: ColumnType.String,
+      age: ColumnType.Integer,
+      birthday: ColumnType.Date,
+      money: ColumnType.Float,
+      update: ColumnType.Timestamp,
+    }
+
+    const parseResult = [
+      {
+        name: 'liming',
+        age: 20,
+        birthday: new Date('2020-07-07'),
+        money: 534.45,
+        update: new Date('2020-08-21 19:29:48'),
+      },
+      {
+        name: 'lili',
+        age: 18,
+        birthday: new Date('2012-06-12'),
+        money: 423.54,
+        update: new Date('2020-08-20 11:31:20'),
+      },
+    ]
+    expect(simpleCsvParser.parseCsv(csv, columnSetting)).toEqual(parseResult)
   })
 })
--- src/utils/simple-csv-parser.ts
+++ src/utils/simple-csv-parser.ts
@@ -1,14 +1,23 @@
+export enum ColumnType {
+  String,
+  Integer,
+  Float,
+  Date,
+  Timestamp,
+}
+
 class SimpleCsvParse {
-  parseCsv(csv: string): object[] {
+  parseCsv(
+    csv: string,
+    ColumnSetting?: { [key: string]: ColumnType }
+  ): object[] {
     const rows = csv.split('\n')
     let data: object[] = []
     let columnMapping: string[] = []
     let isFirstLine: boolean = true
 
     rows.forEach((row) => {
-      // avoid space
       row = row.trim()
-      // skip empty row
       if (!row) {
         return
       }
@@ -20,9 +29,35 @@ class SimpleCsvParse {
         isFirstLine = false
       } else {
         // append data
-        const rowData: { [key: string]: string } = {}
+        const rowData: { [key: string]: any } = {}
         row.split(',').forEach((column, index) => {
-          rowData[columnMapping[index]] = column
+          const columnKey = columnMapping[index]
+          // format by columnSetting
+          if (ColumnSetting) {
+            const columnType = ColumnSetting[columnKey] || ColumnType.String
+            let formatColumn: any
+            switch (columnType) {
+              case ColumnType.String:
+                formatColumn = column
+                break
+              case ColumnType.Integer:
+                formatColumn = parseInt(column, 10)
+                break
+              case ColumnType.Float:
+                formatColumn = parseFloat(column)
+                break
+              case ColumnType.Date:
+                formatColumn = new Date(column)
+                break
+              case ColumnType.Timestamp:
+                // timestamp need change s to ms
+                formatColumn = new Date(parseInt(column, 10) * 1000)
+                break
+            }
+            rowData[columnKey] = formatColumn
+          } else {
+            rowData[columnKey] = column
+          }
         })
         data.push(rowData)
       }
